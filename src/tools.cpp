ad807daf96d8a228782faeac9f25265eed738319
Hristo Vrigazov <hvrigazov@gmail.com>
2017-04-18 21:37:14 +0300

    Small formatting change

--- src/tools.cpp
+++ src/tools.cpp
@@ -18,15 +18,13 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	// check the validity of the following inputs:
 	//  * the estimation vector size should not be zero
 	//  * the estimation vector size should equal ground truth vector size
-	if(estimations.size() != ground_truth.size()
-		 || estimations.size() == 0){
+	if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
 		cout << "Invalid estimation or ground_truth data" << endl;
 		return rmse;
 	}
 
 	//accumulate squared residuals
-	for(unsigned int i=0; i < estimations.size(); ++i){
-
+	for (unsigned int i=0; i < estimations.size(); ++i){
 		VectorXd residual = estimations[i] - ground_truth[i];
 		//coefficient-wise multiplication
 		residual = residual.array()*residual.array();
@@ -55,7 +53,7 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 	double c1 = px*px + py*py;
 
 	//check division by zero
-	if(fabs(c1) < 0.0001){
+	if (fabs(c1) < 10e-6) {
 		cout << "CalculateJacobian () - Error - Division by Zero" << endl;		
 		return Hj;
 	}
